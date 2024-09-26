a3dac2442063fef0ac94580b921996b093d577f0
luka-weweb <luka@weweb.io>
2024-09-26 14:54:25 +0200

    f

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -7,6 +7,7 @@ export default {
   inheritAttrs: false,
   props: {
     content: { type: Object, required: true },
+    uid: { type: String, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
     open: { type: Boolean, required: true },
