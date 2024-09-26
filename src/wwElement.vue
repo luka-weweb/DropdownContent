fde565fd824f3f13682c6cf0676174657db99fca
luka-weweb <luka@weweb.io>
2024-09-26 14:57:47 +0200

    fix

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,16 +1,14 @@
 <template>
-  <wwLayout path="slot" v-bind="$attrs"></wwLayout>
+  <wwLayout path="slot"></wwLayout>
 </template>
 
 <script>
 export default {
-  inheritAttrs: false,
   props: {
     content: { type: Object, required: true },
     uid: { type: String, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
-    open: { type: Boolean, required: true },
   },
 };
 </script>
