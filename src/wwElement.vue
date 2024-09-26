d99c3b3a0e0dd4fdc0fe1f0256fd91021e5490c6
luka-weweb <luka@weweb.io>
2024-09-26 14:53:37 +0200

    ff

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,49 +1,15 @@
 <template>
-  <Teleport
-    :to="this.teleportSettings.teleportTarget"
-    :disabled="!this.teleport"
-    v-if="
-      this.teleport
-        ? this.teleportSettings.isTeleportReady && this.open
-        : this.open
-    "
-  >
-    <wwLayout path="slot" v-bind="$attrs"></wwLayout>
-  </Teleport>
+  <wwLayout path="slot" v-bind="$attrs"></wwLayout>
 </template>
 
 <script>
-import { setupTeleport } from "./teleport.js";
-
 export default {
   inheritAttrs: false,
   props: {
     content: { type: Object, required: true },
     wwFrontState: { type: Object, required: true },
     wwEditorState: { type: Object, required: true },
-    teleport: { type: Boolean, required: true },
     open: { type: Boolean, required: true },
-  },
-  data() {
-    return {
-      teleportSettings: { teleportTarget: null, isTeleportReady: false },
-    };
-  },
-  mounted() {
-    this.$nextTick(() => {
-      if (this.teleport) {
-        this.teleportSettings = setupTeleport();
-      }
-    });
-  },
-  watch: {
-    teleport(newValue) {
-      if (newValue) {
-        this.teleportSettings = setupTeleport();
-      }
-    },
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
