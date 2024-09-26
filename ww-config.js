8f6566574cfe23b449ffd7e9bb94e8edc3cb5e37
luka-weweb <luka@weweb.io>
2024-09-26 15:00:49 +0200

    f

--- ww-config.js
+++ ww-config.js
@@ -28,6 +28,113 @@ export default {
         ],
       },
     },
+    alignment: {
+      label: { en: "Align" },
+      type: "TextRadioGroup",
+      options: (content) => {
+        if (content.position === "top" || content.position === "bottom") {
+          return {
+            choices: [
+              {
+                value: "start",
+                title: { en: "Start" },
+                icon: "align-left",
+              },
+              {
+                value: "center",
+                title: { en: "Center" },
+                icon: "align-center",
+              },
+              { value: "end", title: { en: "End" }, icon: "align-right" },
+            ],
+          };
+        } else {
+          return {
+            choices: [
+              {
+                value: "start",
+                title: { en: "Start" },
+                icon: "align-y-start",
+              },
+              {
+                value: "center",
+                title: { en: "Center" },
+                icon: "align-y-center",
+              },
+              { value: "end", title: { en: "End" }, icon: "align-y-end" },
+            ],
+          };
+        }
+      },
+      defaultValue: "start",
+      classes: true,
+      states: true,
+    },
+    offsetX: {
+      type: "Length",
+      label: {
+        en: "Offset (x)",
+      },
+      bindable: true,
+      options: {
+        unitChoices: [
+          { value: "px", label: "px", min: 1, max: 1000 },
+          { value: "%", label: "%", min: 1, max: 100 },
+        ],
+        noRange: true,
+        useVar: true,
+      },
+      bindingValidation: {
+        type: "string",
+        tooltip: "A string that defines size of offset in px or %",
+      },
+    },
+    offsetY: {
+      type: "Length",
+      label: {
+        en: "Offset (y)",
+      },
+      bindable: true,
+      options: {
+        unitChoices: [
+          { value: "px", label: "px", min: 1, max: 1000 },
+          { value: "%", label: "%", min: 1, max: 100 },
+        ],
+        noRange: true,
+        useVar: true,
+      },
+      bindingValidation: {
+        type: "string",
+        tooltip: "A string that defines size of offset in px or %",
+      },
+    },
+    avoidCollision: {
+      label: {
+        en: "Avoid collision",
+      },
+      type: "OnOff",
+      bindable: true,
+      defaultValue: false,
+    },
+    collisionOffset: {
+      type: "Length",
+      label: {
+        en: "Collision offset",
+      },
+      bindable: true,
+      options: {
+        unitChoices: [
+          { value: "px", label: "px", min: 1, max: 1000 },
+          { value: "%", label: "%", min: 1, max: 100 },
+        ],
+        noRange: true,
+        useVar: true,
+      },
+      bindingValidation: {
+        type: "string",
+        tooltip: "A string that defines size of offset in px or %",
+      },
+    },
     slot: {
       hidden: true,
       defaultValue: [],
